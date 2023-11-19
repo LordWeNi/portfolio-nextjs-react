0eae691f099b562aed234a9c87b22d71b513b89c
Nicolas Weber <weber1310@gmail.com>
2023-11-19 21:21:13 +0100

    footer modified commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2023 Weber Nicolas. All rights reserved.
+        &copy; 2023  Nicolas Weber. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">A propos de ce site:</span> utilisation de
